2aa81fe52419b447ebaad4dcc92dc3a4c04ebd9f
Karolina <karola.rzucidlo14@gmail.com>
2024-11-18 22:03:19 +0100

    feat: add type definitions for app routes

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -1,3 +1,8 @@
+import { BsCashCoin } from "react-icons/bs";
+import { CgNotes, CgProfile } from "react-icons/cg";
+import { GrMoney } from "react-icons/gr";
+import { RxDashboard } from "react-icons/rx";
+
 export function Sidebar() {
   return (
     <>
@@ -5,11 +10,26 @@ export function Sidebar() {
         <h1 className="p-10 text-lg font-bold">BudgetManager </h1>
 
         <ul className="flex flex-col gap-6 ml-4">
-          <li>Dashboard</li>
-          <li>Incomes</li>
-          <li>Budgets</li>
-          <li>Expenses</li>
-          <li>Profile</li>
+          <div className="flex gap-2 items-center">
+            <RxDashboard />
+            <li>Dashboard</li>
+          </div>
+          <div className="flex gap-2 items-center">
+            <BsCashCoin />
+            <li>Incomes</li>
+          </div>
+          <div className="flex gap-2 items-center">
+            <GrMoney />
+            <li>Budgets</li>
+          </div>
+          <div className="flex gap-2 items-center">
+            <CgNotes />
+            <li>Expenses</li>
+          </div>
+          <div className="flex gap-2 items-center">
+            <CgProfile />
+            <li>Profile</li>
+          </div>
         </ul>
       </section>
     </>
---
+++ src/router/routes.ts
@@ -0,0 +1,9 @@
+export type Routes = typeof ROUTES;
+
+export type ROUTES = {
+  dashboard: "/";
+  incomes: "/incomes";
+  budgets: "/budgets";
+  expenses: "/expenses";
+  profile: "/profile";
+};
